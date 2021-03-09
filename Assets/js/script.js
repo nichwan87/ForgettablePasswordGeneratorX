e12986b3477ccc53e47f45ed5befdfdf4ac48493
Nikku <nichwan_is@hotmail.com>
2021-03-09 21:11:46 +1100

    files

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,11 @@
-// Assignment Code
+/* Assignment Code*/
 var generateBtn = document.querySelector("#generate");
 
-// Defining the specialCharacters
+/* Defining the specialCharacters*/
 
 const specialCharacters = "!@#$%^&*()<>.,\?/~-_=+";
 
-// Write password to the #password input
+/* Write password to the #password input*/
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -14,25 +14,25 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+/* Add event listener to generate button*/
 generateBtn.addEventListener("click", writePassword);
 
-// User Prompts
+/* User Prompts*/
 function generatePassword() {
   var passwordLength = prompt("Please enter the number of characters you want your password to be. Please choose from 8 to 128 characters.");
   
   /*if minimum not achieved or maximum exceeded*/
   while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) passwordLength = Number(prompt("This password must be between 8 & 128 characters. Please try again"));
 
-  var numbers = confirm("Do you want to include numbers?");
+  var numbers = confirm("Would you like to include numbers?");
 
-  var lowerCases = confirm("Would you like lowercase letters in your password?");
+  var lowerCases = confirm("Would you like non-capital letters in your password?");
 
-  var upperCases = confirm("Would you like uppercase letters in your password?");
+  var upperCases = confirm("Would you like capital letters in your password?");
 
-  var special = confirm("Do you want special characters added?");
+  var special = confirm("Would you also like to use special characters?");
 
-  /*if none of the above are selected*/
+  /*If none of the above are selected*/
   while (!upperCases && !lowerCases && !numbers && !special) {
     alert("You must select at least one character type!");
     numbers = confirm("Would you like to now choose to have numbers?");
@@ -41,17 +41,15 @@ function generatePassword() {
     special = confirm("Would you like to now choose to have special characters?");
   }
 
-  // Setting minium amount of the selection
+  /* Setting minium amount of the selection*/
   var minimumCount = 0;
-
-  // Empty selection minimums for numbers, lowerCases, upperCases & specialCharacters to aid in output
 
   var minimumNumbers = "";
   var minimumLowerCases = "";
   var minimumUpperCases = "";
   var minimumSpecialCharacters = "";
 
-  // Generator functions
+  /* Generator functions*/
   var functionArray = {
     getNumbers: function() {
       return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
@@ -70,7 +68,7 @@ function generatePassword() {
     }
 };
 
-  // If user selected ok for below, will use empty minimums from above
+  /* If user selected ok for below, will use empty minimums from above*/
 
   if (numbers === true) {
     minimumNumbers = functionArray.getNumbers();
@@ -92,23 +90,22 @@ function generatePassword() {
     minimumCount++;
   }
 
-  // empty string for the below to reference off
+  /* empty string for the below to reference off*/
   var randomPasswordGenerated = "";
 
-  // using the above, use the below to loop around ramdom characters and symbols
+ 
   for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
     var randomNumberPicked = Math.floor(Math.random() * 9);
 
     randomPasswordGenerated += randomNumberPicked;
   }
 
-  // adding random generated characters to output
   randomPasswordGenerated += minimumNumbers;
   randomPasswordGenerated += minimumLowerCases;
   randomPasswordGenerated += minimumUpperCases;
   randomPasswordGenerated += minimumSpecialCharacters;
 
-  //adding the output
+  /*Adding the output*/
   return randomPasswordGenerated;
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # ForgettablePasswordGeneratorX
+This is a password generator that will provide users with a random password. The password is randomly generated and thus, may be difficult to remember.
+
+# How to use
+First click on the "Generate Password" button
+Then follow prompts to generate your password
+The password must contain between 8 and 128 character. Users must also select at least one option under the Numbers, upperCases, lowerCases and specialCharacters.
+NB: The box where the password generates to is not an Open Field Text Input Section - users cannot type in the box.
+
+# Usage
+Access the current output via (https://nichwan87.github.io/ForgettablePasswordGeneratorX/) and view the expected output.
+
+# Support
+Please contact nichwan_is@hotmail.com for any support related enquiries.
+
+# Roadmap
+Future updates will include additional functionality to limit the ratio of number to other characters generated per password
+
+# Contributing
+Pull requests are welcome with tracking. Any major changes are requested to be approved via the support email - (nichwan_is@hotmail.com)
+
+# Acknowledgement
+Thank you to Monash Bootcamp for providing files to work through
+Thank you to stackoverflow and w3schools for additional resources to work through this with
+
+# License
+&copy Monash Bootcamp homework assignment 3
+https://nichwan87.github.io/ForgettablePasswordGeneratorX/
